af26c0d4fe3ef5959baabafdb9373496bb397689
Ángel Díaz <diazangel9311@gmail.com>
2024-07-13 10:40:09 -0500

    Implement lists section

---
+++ src/app/(main)/home/@lists/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <article className="bg-neutral-200 h-full p-3 rounded-sm">
+      <h3>Lists</h3>
+    </article>
+  );
+}
--- src/app/(main)/home/layout.tsx
+++ src/app/(main)/home/layout.tsx
@@ -2,16 +2,21 @@ export default function HomeLayout({
   children,
   lastbudgetperiods,
   currentbudgetperiod,
+  lists,
 }: Readonly<{
   children: React.ReactNode;
   lastbudgetperiods: React.ReactNode;
   currentbudgetperiod: React.ReactNode;
+  lists: React.ReactNode;
 }>) {
   return (
-    <div>
-      <section className="grid grid-flow-row lg:grid-flow-col gap-2">
+    <div className="grid grid-flow-row gap-3">
+      <section className="grid grid-flow-row lg:grid-flow-col gap-3">
         <div className="lg:col-span-4">{currentbudgetperiod}</div>
         <div className="lg:col-span-8">{lastbudgetperiods}</div>
+      </section>
+      <section className="grid grid-flow-row lg:grid-flow-col lg:grid-cols-12 gap-3">
+        <div className="lg:col-span-6">{lists}</div>
       </section>
       {children}
     </div>
