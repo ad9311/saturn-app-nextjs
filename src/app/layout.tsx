76f15d31f351b31ae31bb9ae67b841f8c107ca1b
Ángel Díaz <diazangel9311@gmail.com>
2024-07-19 13:23:58 -0500

    Install react-query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import SessionProvider from './auth/sign-in/SessionProvider';
 import './globals.css';
+import ClientProvider from '@/providers/ClientProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,10 +15,12 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <SessionProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
-    </SessionProvider>
+    <html lang="en">
+      <body className={inter.className}>
+        <SessionProvider>
+          <ClientProvider>{children}</ClientProvider>
+        </SessionProvider>
+      </body>
+    </html>
   );
 }
