e180c517676927c3f639c08b12fdde046d8c1e59
Ángel Díaz <diazangel9311@gmail.com>
2024-07-25 21:45:21 -0500

    Modify middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,11 @@
-export { auth as middleware } from '@/auth';
+import { auth } from "@/auth"
+
+export default auth((request) => {
+  if (!request.auth && request.nextUrl.pathname !== "/auth/sign-in") {
+    const newUrl = new URL("/auth/sign-in", request.nextUrl.origin)
+    return Response.redirect(newUrl)
+  }
+})
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
