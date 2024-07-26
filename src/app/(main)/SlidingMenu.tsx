0e948af6005bb1e11711d8b4454e77fb705f03f7
Ángel Díaz <diazangel9311@gmail.com>
2024-07-25 22:30:58 -0500

    Implement close sliding bar when clicking a link

--- src/app/(main)/SlidingMenu.tsx
+++ src/app/(main)/SlidingMenu.tsx
@@ -3,9 +3,10 @@
 import { User } from '@prisma/client';
 import Image from 'next/image';
 import Link from 'next/link';
-
-import { useSlidingMenuStore } from '@/stores/sliding-menu';
 import { useEffect } from 'react';
+
+import GearImage from '@/assets/img/gear.svg';
+import { useSlidingMenuStore } from '@/stores/sliding-menu';
 
 const links = [
   { body: 'Home', path: '/home' },
@@ -14,7 +15,10 @@ const links = [
 ];
 
 export default function SlidingMenu({ user }: { user: User }) {
-  const { open } = useSlidingMenuStore(state => ({ open: state.open }));
+  const { open, setToggle } = useSlidingMenuStore(state => ({
+    open: state.open,
+    setToggle: state.setToggle,
+  }));
 
   function handleClickOutside(event: MouseEvent) {
     const slidingMenuContainer = document.getElementById('sliding-menu-container');
@@ -24,38 +28,46 @@ export default function SlidingMenu({ user }: { user: User }) {
     if (slidingMenuContainer && slidingMenu) {
       const clickOutside = !slidingMenuContainer.contains(target);
       if (clickOutside && slidingMenu.classList.contains('left-0')) {
-        slidingMenu.classList.toggle('-left-72')
-        slidingMenu.classList.toggle('left-0')
+        setToggle();
+        // slidingMenu.classList.toggle('-left-72')
+        // slidingMenu.classList.toggle('left-0')
       }
     }
   }
 
   useEffect(() => {
-    document.addEventListener('click', handleClickOutside)
+    document.addEventListener('click', handleClickOutside);
 
-    return (() => {
+    return () => {
       window.removeEventListener('click', handleClickOutside);
-    })
-  })
+    };
+  });
 
   const mappedLinks = links.map(link => (
     <li key={link.path}>
-      <Link href={link.path} className="nav-link">
+      <Link href={link.path} className="nav-link" onClick={setToggle}>
         <span className="text-lg">{link.body}</span>
       </Link>
     </li>
   ));
 
   return (
-    <div id="sliding-menu" className={`absolute card h-[90vh] w-72 transition-all ${open ? 'left-0' : '-left-72'}`}>
+    <div
+      id="sliding-menu"
+      className={`absolute card h-[90vh] w-72 transition-all ${open ? 'left-0' : '-left-72'}`}>
       <div className="mt-10 text-center">
-        <Image
-          src={user.image}
-          alt="profile-picture"
-          width={100}
-          height={100}
-          className="w-fit mx-auto mb-5 rounded-full"
-        />
+        <div className="relative">
+          <Image
+            src={user.image}
+            alt="profile-picture"
+            width={100}
+            height={100}
+            className="w-fit mx-auto mb-5 rounded-full"
+          />
+          <Link href="/profile" className="absolute bottom-1 right-16" onClick={setToggle}>
+            <Image src={GearImage} alt="gear" className="w-8" />
+          </Link>
+        </div>
         <p className="title">{user.name}</p>
         <ul className="mt-20 flex flex-col gap-5">{mappedLinks}</ul>
       </div>
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,11 @@
-import { auth } from "@/auth"
+import { auth } from '@/auth';
 
-export default auth((request) => {
-  if (!request.auth && request.nextUrl.pathname !== "/auth/sign-in") {
-    const newUrl = new URL("/auth/sign-in", request.nextUrl.origin)
-    return Response.redirect(newUrl)
+export default auth(request => {
+  if (!request.auth && request.nextUrl.pathname !== '/auth/sign-in') {
+    const newUrl = new URL('/auth/sign-in', request.nextUrl.origin);
+    return Response.redirect(newUrl);
   }
-})
+});
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
