8a83a602dbe3afc48b68e4a816a06b87905fb423
Ángel Díaz <diazangel9311@gmail.com>
2024-07-25 19:29:18 -0500

    Add navbar icon

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -1,10 +1,13 @@
+import Image from 'next/image';
 import Link from 'next/link';
+import SquareGrid from '@/assets/img/square-grid.svg';
 
 export default function Navbar(props: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div {...props}>
-      <Link href="/home">Home</Link>
-      <Link href="/profile">Profile</Link>
+      <Image src={SquareGrid} alt="menu" className="w-7" />
+      {/* <Link href="/home">Home</Link>
+      <Link href="/profile">Profile</Link> */}
     </div>
   );
 }
